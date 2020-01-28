5fceb18bb8d777162663b7acee72864262cac30d
Amin <yousefizadeamin@gmail.com>
2020-01-29 02:06:12 +0330

    package propery class is created

---
+++ PackageProperty.py
@@ -0,0 +1,6 @@
+class PackageProperty:
+    def __init__(self):
+        pass
+    # this class is created to calculate properties based on Eos
+    # object of this class can be srk and pr
+    # to make every things simple we use one of them
