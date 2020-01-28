98e8abdb16c6ff532e6efdaa55cbc0a515abcb67
Amin <yousefizadeamin@gmail.com>
2020-01-29 02:12:39 +0330

    mixture component is added

---
+++ MixtureComponent.py
@@ -0,0 +1,7 @@
+import Component
+
+
+class MixtureComponent(Component):
+    def __init__(self):
+        pass
+    # initial props in this kind of components are less than others and they are calculable
