408c8c57a52d4b74e7013f34d7b1c15cd5d82d43
Amin <yousefizadeamin@gmail.com>
2020-01-29 02:08:43 +0330

    pure component class is created

---
+++ PureComponent.py
@@ -0,0 +1,7 @@
+import Component
+
+
+class PureComponent(Component):
+    def __init__(self):
+        pass
+    # in this class we use serialization for some very usable components such as water etc.
