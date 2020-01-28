38d8e4adf51258773399792b2fc5f613ddd95c2b
Amin <yousefizadeamin@gmail.com>
2020-01-29 01:56:38 +0330

    Fluid is added

---
+++ Fluid.py
@@ -0,0 +1,10 @@
+import ProperyContainer
+
+
+class Fluid(ProperyContainer):
+    # every changes execute on this class via main update
+    # some properties like Re and Nu is calculated from here
+    # fluid is the class which use package property to change and update phases!!!
+    # some efficient prop from different phases must be calculate like efficient viscosity
+    # this class has a lot of setter and getter and this class is the most concentrated class in this lib
+    pass
