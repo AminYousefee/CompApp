61a67d587ffd318c5f4961eacc74b237990ec66a
Amin <yousefizadeamin@gmail.com>
2020-01-30 17:44:54 +0330

    pickle test is completed

--- test_Amin.py
+++ test_Amin.py
@@ -1,3 +1,4 @@
+import pickle
 class Test:
     counter = 0
 
@@ -11,6 +12,7 @@ class Test:
 
     @name.setter
     def name(self, val):
+        self.__name = val
         print("name setter")
 
 
@@ -18,3 +20,15 @@ a = Test("Mina")
 print(a.name)
 a.name = "Amin"
 print(a.name)
+b = Test("mammad")
+print(b.name)
+with open("pickle_test.dat", "wb") as file:
+    pickle.dump(a, file)
+    print("a is pickled")
+
+with open("pickle_test.dat", "rb") as f:
+    b = pickle.load(f)
+    print("b ia like a and it's name must be Amin")
+    print(b.name)
+
+print(b.name)
