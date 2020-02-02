1c84549bd5d39fbfac15da5e229262121543392c
Amin <yousefizadeamin@gmail.com>
2020-02-02 20:05:18 +0330

    initial Phase.py

--- GasPhase.py
+++ GasPhase.py
@@ -1,6 +1,7 @@
-import Phase
+from Phase import Phase
 
 
 class GasPhase(Phase):
-    def __init__(self):
-        pass
+    def __init__(self, fluid, compositions):
+        super().__init__(fluid)
+        self.__compositions = compositions
