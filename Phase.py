a131afacb540d3e7ec98677b55c585a175fea6b8
CNPW1096 <emadimina76@gmail.com>
2020-02-03 13:52:25 +0330

    calculation of entropy for gas phase
    Signed-off-by: CNPW1096 <emadimina76@gmail.com>

--- Phase.py
+++ Phase.py
@@ -1,8 +1,2 @@
-import ProperyContainer
-
-
-class Phase(ProperyContainer):
-    def __init__(self, fluid):
-        self.__fluid = fluid
-    # we have to put a lot of setter and getter
-    # also must add some calculate methods
+# we have to put a lot of setter and getter
+# also must add some calculate methods
